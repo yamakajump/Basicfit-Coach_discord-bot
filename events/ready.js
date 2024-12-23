a10a94e80ca4854b5e710deb225c35456f3a8f95
yamakajump <corentin.batard2003@gmail.com>
2024-12-23 15:05:11 +0100

    Envoyer un message dans le salon général lors du démarrage du bot

--- events/ready.js
+++ events/ready.js
@@ -8,5 +8,11 @@ module.exports = {
     execute(client) {
         console.log(`Connecté en tant que ${client.user.tag}`);
         console.log('Le bot est prêt !');
+
+        // envoie un message dans le salon de général à chaque démarrage du bot
+        const generalChannel = client.channels.cache.get('1308349548711907360');
+        if (generalChannel) {
+            generalChannel.send('Le bot est redémarré !');
+        }
     },
 };
