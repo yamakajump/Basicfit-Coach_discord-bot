679f60e4da8bf372cdce871f592141f9287b954c
CyberTrist <114986392+CyberSpaceRS@users.noreply.github.com>
2024-12-27 16:09:47 +0100

    Update imc.js

--- commands/imc.js
+++ commands/imc.js
@@ -32,6 +32,6 @@ module.exports = {
         else classification = "Obésité";
 
         // Réponse
-        await interaction.reply(`\<a:rouge:843212747998232666> **Résultat de votre IMC** :\n\n- **IMC** : ${imc}\n- **Classification** : ${classification}`);
+        await interaction.reply(`\<:info:1322215662621425674> **Résultat de votre IMC** :\n\n- **IMC** : ${imc}\n- **Classification** : ${classification}`);
     },
 };
