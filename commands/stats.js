2ed59211697c7ae9e22c044c1655ca89b7bf5ce7
yamakajump <corentin.batard2003@gmail.com>
2025-01-06 15:56:07 +0100

    Ajout de l'AttachmentBuilder pour la gestion des pièces jointes dans la commande stats

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const { createCanvas } = require('canvas');
